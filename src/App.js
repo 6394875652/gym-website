6a24dcb151d7be6c35cc204c0010ac061de213b8
ANAND KUMAR <anandk27375@gmail.com>
2025-03-05 00:11:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import './components/Navbar.css'
+import './components/Banner.css'
+import './components/GymClass.css'
+import './components/About.css'
+import './components/ChoseUs.css'
+import './components/Team.css'
+import './components/Subscribe.css'
+import './components/MonthlyPlan.css'
+import './components/BMI.css'
+import './components/Testimonial.css'
+import './components/Footer.css'
+import './components/PageHeader.css'
+import './components/Features.css'
+import './components/Services.css'
+import './components/Contact.css'
+
+import './responsive.css'
+
+import Navbar from './components/Navbar';
+import HomeNav from "./routes/HomeNav";
+import AboutNav from "./routes/AboutNav";
+import FeaturesNav from "./routes/FeaturesNav";
+import ContactNav from "./routes/ContactNav";
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<HomeNav />}></Route>
+        <Route path="/about" element={<AboutNav />}></Route>
+        <Route path="/features" element={<FeaturesNav />}></Route>
+        <Route path="/contact" element={<ContactNav />}></Route>
+
+      </Routes>
+
+      <Footer />
+
+    </>
   );
 }
 
